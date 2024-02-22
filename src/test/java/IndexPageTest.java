28d60af67f636df785af8ebe7a5c862d454bd540
Sara Paul <paul.sara@gmail.com>
2024-02-22 12:34:12 +0530

    Sara | Add test to validate index page

---
+++ src/test/java/IndexPageTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class IndexPageTest {
+    private ChromeDriver chromeDriver;
+    @BeforeEach
+    public void init() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("headless");
+        chromeDriver = new ChromeDriver(chromeOptions);
+    }
+
+    @AfterEach
+    public void close() {
+        chromeDriver.close();
+    }
+    @Test
+    public void expectIndexPageToContainHeading() {
+        chromeDriver.get("http://localhost");
+        assertThat(heading()).isEqualTo("Office Lunch");
+    }
+
+    private String heading() {
+        return chromeDriver.findElement(By.id("title")).getText();
+    }
+}
